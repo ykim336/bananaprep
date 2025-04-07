7d74f1ddaa8d44f0596e53085431f0e0e2e46649
nander100 <alex.totah278@gmail.com>
2025-04-07 16:07:30 -0700

    Run the docker file with docker compose and run the client side through the shell

--- build-and-run.sh
+++ build-and-run.sh
@@ -8,6 +8,9 @@ docker build -t bananaprep .
 echo "Starting BananaPrep container..."
 docker run -p 3000:3000 -d --name bananaprep-app bananaprep
 
-echo "BananaPrep is now running at http://localhost:3000"
+echo "BananaPrep server is now running at http://localhost:3000"
 echo "To view container logs: docker logs bananaprep-app"
 echo "To stop the container: docker stop bananaprep-app"
+
+python -m http.server 8080
+echo "BananaPrep client is now running at http://localhost:8080"
