25808105e37fd2f3612309bcc0ecc95845e6f0c7
nander100 <alex.totah278@gmail.com>
2025-04-09 00:21:55 -0700

    Fix Python command in server_run.sh and update startup message

--- server_run.sh
+++ server_run.sh
@@ -7,7 +7,7 @@ docker run -p 3000:3000 -d --name bananaprep-app bananaprep
 # Start the frontend using Python HTTP server in the client directory
 echo "Starting BananaPrep client at http://localhost:8000"
 cd ~/bananaprep/client || exit 1
-python -m http.server 8000 &
+python3 -m http.server 8000 &
 CLIENT_PID=$!
 
 echo ""
